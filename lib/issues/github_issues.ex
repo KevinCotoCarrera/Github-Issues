9c32dd5ded185509f727f9bf7d80af8dd2530315
Kevin Coto <kevin.coto@enerex.com>
2022-09-14 12:53:02 -0400

    added-fetch-and-config_file

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,24 @@
+defmodule Issues.GithubIssues do
+  @user_agent [  {"User-agent", "Elixir dave@pragprog.com"}]
+
+  def fetch(user,project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  #use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+  def issues_url(user,project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({_ , %{status_code: status_code, body: body}}) do
+    { status_code |> check_for_error() ,
+      body        |> Poison.Parser.parse!()
+    }
+  end
+
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_),   do: :error
+end
