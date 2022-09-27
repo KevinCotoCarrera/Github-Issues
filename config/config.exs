b9abc9df6a1d9c0ba6c5995a54d73e52746ff9f3
Kevin Coto <kevincoto2002@gmail.com>
2022-09-26 21:56:08 -0400

    Logger Functionalities

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,5 @@
 import Config
+
 config :issues, github_url: "https://api.github.com"
+
+config :logger, compile_time_purge_level: :info
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,7 +1,11 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [  {"User-agent", "Elixir dave@pragprog.com"}]
 
   def fetch(user,project) do
+    Logger.info("Fetching #{user}'s project #{project}")
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -14,6 +18,8 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_ , %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
     { status_code |> check_for_error() ,
       body        |> Poison.Parser.parse!()
     }
